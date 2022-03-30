a2483d5692b9586ac8ea80acbdd06ce26eab5162
onurpskll <onurpskll@gmail.com>
2022-03-30 21:59:04 +0300

    Alışveriş

---
+++ Alisveris/MainV.Designer.cs
@@ -0,0 +1,405 @@
+﻿
+namespace Alisveris
+{
+    partial class MainV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSepetTutar = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lboSepetDetay = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBirimTutar = new System.Windows.Forms.TextBox();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.txtUrunAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblGunlukCiro = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCiroHedef = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cboPersonelIndirim = new System.Windows.Forms.ComboBox();
+            this.cboIskonto = new System.Windows.Forms.ComboBox();
+            this.cboKdv = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnUrunEkle = new System.Windows.Forms.Button();
+            this.btnUrunSil = new System.Windows.Forms.Button();
+            this.btnYeniKayit = new System.Windows.Forms.Button();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSepetTutar);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lboSepetDetay);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtBirimTutar);
+            this.groupBox1.Controls.Add(this.txtMiktar);
+            this.groupBox1.Controls.Add(this.txtUrunAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 289);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Alışveriş";
+            // 
+            // lblSepetTutar
+            // 
+            this.lblSepetTutar.AutoSize = true;
+            this.lblSepetTutar.Location = new System.Drawing.Point(148, 251);
+            this.lblSepetTutar.Name = "lblSepetTutar";
+            this.lblSepetTutar.Size = new System.Drawing.Size(0, 13);
+            this.lblSepetTutar.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Sepet Toplam Tutar";
+            // 
+            // lboSepetDetay
+            // 
+            this.lboSepetDetay.FormattingEnabled = true;
+            this.lboSepetDetay.Location = new System.Drawing.Point(29, 134);
+            this.lboSepetDetay.Name = "lboSepetDetay";
+            this.lboSepetDetay.Size = new System.Drawing.Size(240, 108);
+            this.lboSepetDetay.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sepet Detayı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Birim Tutar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Miktarı";
+            // 
+            // txtBirimTutar
+            // 
+            this.txtBirimTutar.Location = new System.Drawing.Point(133, 84);
+            this.txtBirimTutar.Name = "txtBirimTutar";
+            this.txtBirimTutar.Size = new System.Drawing.Size(136, 20);
+            this.txtBirimTutar.TabIndex = 3;
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(133, 55);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.Size = new System.Drawing.Size(136, 20);
+            this.txtMiktar.TabIndex = 2;
+            // 
+            // txtUrunAd
+            // 
+            this.txtUrunAd.Location = new System.Drawing.Point(133, 29);
+            this.txtUrunAd.Name = "txtUrunAd";
+            this.txtUrunAd.Size = new System.Drawing.Size(136, 20);
+            this.txtUrunAd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblGunlukCiro);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtCiroHedef);
+            this.groupBox2.Location = new System.Drawing.Point(0, 295);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(323, 119);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ciro Bilgileri";
+            // 
+            // lblGunlukCiro
+            // 
+            this.lblGunlukCiro.AutoSize = true;
+            this.lblGunlukCiro.Location = new System.Drawing.Point(130, 84);
+            this.lblGunlukCiro.Name = "lblGunlukCiro";
+            this.lblGunlukCiro.Size = new System.Drawing.Size(13, 13);
+            this.lblGunlukCiro.TabIndex = 10;
+            this.lblGunlukCiro.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Günlük Ciro";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Ciro Hedefi";
+            // 
+            // txtCiroHedef
+            // 
+            this.txtCiroHedef.Enabled = false;
+            this.txtCiroHedef.Location = new System.Drawing.Point(112, 33);
+            this.txtCiroHedef.Name = "txtCiroHedef";
+            this.txtCiroHedef.ReadOnly = true;
+            this.txtCiroHedef.Size = new System.Drawing.Size(136, 20);
+            this.txtCiroHedef.TabIndex = 10;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cboPersonelIndirim);
+            this.groupBox3.Controls.Add(this.cboIskonto);
+            this.groupBox3.Controls.Add(this.cboKdv);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Location = new System.Drawing.Point(352, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(287, 134);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kesintiler";
+            // 
+            // cboPersonelIndirim
+            // 
+            this.cboPersonelIndirim.FormattingEnabled = true;
+            this.cboPersonelIndirim.Items.AddRange(new object[] {
+            "0",
+            "5",
+            "10",
+            "15",
+            "20"});
+            this.cboPersonelIndirim.Location = new System.Drawing.Point(125, 82);
+            this.cboPersonelIndirim.Name = "cboPersonelIndirim";
+            this.cboPersonelIndirim.Size = new System.Drawing.Size(121, 21);
+            this.cboPersonelIndirim.TabIndex = 18;
+            // 
+            // cboIskonto
+            // 
+            this.cboIskonto.FormattingEnabled = true;
+            this.cboIskonto.Items.AddRange(new object[] {
+            "0",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50"});
+            this.cboIskonto.Location = new System.Drawing.Point(125, 55);
+            this.cboIskonto.Name = "cboIskonto";
+            this.cboIskonto.Size = new System.Drawing.Size(121, 21);
+            this.cboIskonto.TabIndex = 17;
+            // 
+            // cboKdv
+            // 
+            this.cboKdv.FormattingEnabled = true;
+            this.cboKdv.Items.AddRange(new object[] {
+            "1",
+            "8",
+            "18"});
+            this.cboKdv.Location = new System.Drawing.Point(125, 28);
+            this.cboKdv.Name = "cboKdv";
+            this.cboKdv.Size = new System.Drawing.Size(121, 21);
+            this.cboKdv.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(18, 86);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Personel İndirimi";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(18, 59);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "İskonto";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(18, 31);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 13);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "KDV";
+            // 
+            // btnUrunEkle
+            // 
+            this.btnUrunEkle.Location = new System.Drawing.Point(352, 167);
+            this.btnUrunEkle.Name = "btnUrunEkle";
+            this.btnUrunEkle.Size = new System.Drawing.Size(79, 23);
+            this.btnUrunEkle.TabIndex = 13;
+            this.btnUrunEkle.Text = "Ürün Ekle";
+            this.btnUrunEkle.UseVisualStyleBackColor = true;
+            this.btnUrunEkle.Click += new System.EventHandler(this.btnUrunEkle_Click);
+            // 
+            // btnUrunSil
+            // 
+            this.btnUrunSil.Location = new System.Drawing.Point(453, 167);
+            this.btnUrunSil.Name = "btnUrunSil";
+            this.btnUrunSil.Size = new System.Drawing.Size(84, 23);
+            this.btnUrunSil.TabIndex = 14;
+            this.btnUrunSil.Text = "Ürün Sil";
+            this.btnUrunSil.UseVisualStyleBackColor = true;
+            this.btnUrunSil.Click += new System.EventHandler(this.btnUrunSil_Click);
+            // 
+            // btnYeniKayit
+            // 
+            this.btnYeniKayit.Location = new System.Drawing.Point(559, 167);
+            this.btnYeniKayit.Name = "btnYeniKayit";
+            this.btnYeniKayit.Size = new System.Drawing.Size(80, 23);
+            this.btnYeniKayit.TabIndex = 15;
+            this.btnYeniKayit.Text = "Yeni Kayıt";
+            this.btnYeniKayit.UseVisualStyleBackColor = true;
+            this.btnYeniKayit.Click += new System.EventHandler(this.btnYeniKayit_Click);
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMesaj.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblMesaj.Location = new System.Drawing.Point(347, 328);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(0, 25);
+            this.lblMesaj.TabIndex = 12;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 424);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.btnYeniKayit);
+            this.Controls.Add(this.btnUrunSil);
+            this.Controls.Add(this.btnUrunEkle);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBirimTutar;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.TextBox txtUrunAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lboSepetDetay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSepetTutar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCiroHedef;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblGunlukCiro;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnUrunEkle;
+        private System.Windows.Forms.Button btnUrunSil;
+        private System.Windows.Forms.Button btnYeniKayit;
+        private System.Windows.Forms.ComboBox cboPersonelIndirim;
+        private System.Windows.Forms.ComboBox cboIskonto;
+        private System.Windows.Forms.ComboBox cboKdv;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Alisveris/MainV.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alisveris
+{
+    public partial class MainV : Form
+    {
+        public MainV()
+        {
+            InitializeComponent();
+        }
+
+        void Temizle()
+        {
+            txtBirimTutar.Clear(); txtMiktar.Clear(); txtUrunAd.Clear();
+            lboSepetDetay.Items.Clear();
+            cboKdv.SelectedIndex = -1; cboIskonto.SelectedIndex = 0; cboPersonelIndirim.SelectedIndex = 0;
+            lblSepetTutar.Text = "";
+        }
+
+        const double ciroHedef = 500; // sabit değer
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lboSepetDetay.Items.Add("Fiş No\tÜrün Adı\tFiyat");
+
+            txtCiroHedef.Text = ciroHedef.ToString();
+
+            cboIskonto.SelectedIndex = 0;
+            cboPersonelIndirim.SelectedIndex = 0;
+        }
+
+       
+        double sepetToplam = 0;
+
+        private void btnUrunEkle_Click(object sender, EventArgs e)
+        {
+            if (txtUrunAd.Text == "" || txtMiktar.Text == "" || txtBirimTutar.Text == "" || cboKdv.SelectedIndex == -1)
+            {
+                MessageBox.Show("Ürün eklemek için ilgili alanları doldurunuz..");
+            }
+            else
+            {
+                string urunAd = txtUrunAd.Text;
+                double miktar = Convert.ToDouble(txtMiktar.Text);
+                double birimTutar = Convert.ToDouble(txtBirimTutar.Text);
+                double toplamTutar = miktar * birimTutar;       
+                int kdv =  Convert.ToInt32(cboKdv.Text);
+                int iskonto =  Convert.ToInt32(cboIskonto.Text);
+                int personelIndirim =  Convert.ToInt32(cboPersonelIndirim.Text);
+                toplamTutar = toplamTutar + ((toplamTutar * kdv) / 100) - ((toplamTutar * iskonto) / 100) - ((toplamTutar * personelIndirim) / 100);
+                sepetToplam += toplamTutar;
+                lboSepetDetay.Items.Add(urunNumara + "\t" + urunAd + "\t" + toplamTutar);
+
+                lblSepetTutar.Text = sepetToplam.ToString();
+            }
+        }
+
+        private void btnUrunSil_Click(object sender, EventArgs e)
+        {
+            int secilen = lboSepetDetay.SelectedIndex;
+            if (secilen != -1)
+            {
+                lboSepetDetay.Items.RemoveAt(secilen);
+                MessageBox.Show("Seçilen ürün başarıyla silindi!");
+            }
+            else
+            {
+                MessageBox.Show("Seçim Yapın!");
+            }
+        }
+
+        int urunNumara = 1;
+        private void btnYeniKayit_Click(object sender, EventArgs e)
+        {
+            lblGunlukCiro.Text = sepetToplam.ToString();
+            ++urunNumara;
+            Temizle();
+
+            if (sepetToplam >= ciroHedef)
+            {
+                lblMesaj.Text = "GÜNLÜK CİROYA ULAŞILDI!";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = $"Alışveriş Uygulaması | {DateTime.Now}";
+        }
+    }
+}
